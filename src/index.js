4a430f9a054de1e4640b2d6c15e75dfbd0c9a852
Lise Hove <lisehove@hotmail.com>
2020-09-22 19:19:31 +0200

    Remove import of css in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import './Bar.css';
 
 export default class Bar extends Component {
-	state = { width: 0 }
+	state = { width: 0 };
 	componentDidMount() {
 		this.setState({ width: this.holder.clientWidth });
 	}
@@ -12,50 +11,45 @@ export default class Bar extends Component {
 		const fontSize = (this.props.labelSize / 100) * this.state.width;
 		const style = { color: this.props.labelColor };
 		return this.props.labels.map((label, i) => (
-			<span
-				style={{ left: `${(dist * i)}%`, ...style }}
-				className="label"
-				key={label}
-			>
+			<span style={{ left: `${dist * i}%`, ...style }} className='label' key={label}>
 				{label}
 			</span>
-		))
+		));
 	}
 
 	renderBars() {
 		const dist = 100 / (this.props.labels.length - 1);
-		const array = this.props.labels.map((label, i) => (
+		const array = this.props.labels.map((label, i) =>
 			i === 0 || i === this.props.labels.length - 1 ? null : (
 				<div
 					style={{
-						left: `${(dist * i)}%`,
+						left: `${dist * i}%`,
 						backgroundColor: this.props.seperatorColor,
 					}}
-					className="label progress-bar__seperator"
+					className='label progress-bar__seperator'
 					key={label}
-				/>)
+				/>
 			)
-		)
+		);
 		return array;
 	}
 
 	render() {
-		const {
-			progress,
-			barColor,
-			label,
-		} = this.props;
+		const { progress, barColor, label } = this.props;
 		const style = {
 			width: `${progress}%`,
 			backgroundColor: barColor,
 		};
 		return (
-			<div ref={holder => {this.holder = holder}} className="progress-bar">
-				<div className="progress-bar__label-holder">
-					{this.renderLabel()}
-				</div>
-				<div className="progress-bar__progress-bg">
-					<div className="progress-bar__progress" style={style}/>
+			<div
+				ref={(holder) => {
+					this.holder = holder;
+				}}
+				className='progress-bar'
+			>
+				<div className='progress-bar__label-holder'>{this.renderLabel()}</div>
+				<div className='progress-bar__progress-bg'>
+					<div className='progress-bar__progress' style={style} />
 					{this.renderBars()}
 				</div>
 			</div>
